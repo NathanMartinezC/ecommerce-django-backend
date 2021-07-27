7ea44f10e0d40e85aac42a069b8271c82d47fae6
Nathan Martinez <n.mtz.cas@gmail.com>
2021-07-26 19:27:04 -0500

    Added rating and carousel

--- base/views/order_views.py
+++ base/views/order_views.py
@@ -65,6 +65,14 @@ def getMyOrders(request):
 
 
 @api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getOrders(request):
+    orders = Order.objects.all()
+    serializer = OrderSerializer(orders, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def getOrderById(request, pk):
     user = request.user
@@ -88,3 +96,13 @@ def updateOrderToPaid(request, pk):
     order.paid_at = datetime.now()
     order.save()
     return Response('Order was paid')
+
+
+@api_view(['PUT'])
+@permission_classes([IsAdminUser])
+def updateOrderToDelivered(request, pk):
+    order = Order.objects.get(_id=pk)
+    order.is_delivered = True
+    order.delivered_at = datetime.now()
+    order.save()
+    return Response('Order was delivered')
